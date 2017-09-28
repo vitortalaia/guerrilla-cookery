83ba70fd132e521eaad35ed7655571b01d8c48d7
Vitor Talaia <vitor.talaia.oliveira@gmail.com>
2017-09-28 13:22:05 -0300

    Style the main component and create a header for the application

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
+import Header from '../components/header'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1>Guerrilla Cookery</h1>
-        </header>
+      <div className="container">
+        <Header />
       </div>
     )
   }
