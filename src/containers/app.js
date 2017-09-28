870ae17cc2425054633907ad9cafd6b64b9100d3
Vitor Talaia <vitor.talaia.oliveira@gmail.com>
2017-09-28 13:48:32 -0300

    Create a loader component

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 import Header from '../components/header'
+import Loader from '../components/loader'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isFetching: false
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <Header />
+
+        { this.state.isFetching && <Loader /> }
       </div>
     )
   }
